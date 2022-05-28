6d8bcbfb7c178a7f2b6ac8bebfbfa0fa07f3413b
saintic <saintic132@gmail.com>
2022-05-28 18:27:40 +0300

    Тест !!!11111123123

--- src/components/features/Login/Login.tsx
+++ src/components/features/Login/Login.tsx
@@ -3,6 +3,11 @@ export const Login = () => {
     return (
         <div>
             Login 123
+            rdwef
+             wef
+             wef
+             we
+            f
         </div>
     )
 };
