9a9ec76eb6866c71ee29e1a21cd46a21385bb499
saintic <saintic132@gmail.com>
2022-05-30 21:03:05 +0300

    Сделана компонента Profile по макету

--- src/components/Container/Main/Profile/ProfilePacksList/Packs/PackList.tsx
+++ src/components/Container/Main/Profile/ProfilePacksList/Packs/PackList.tsx
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './PackList.module.css'
+import SuperButton from "../../../../../../common/buttons/c2-SuperButton/SuperButton";
 
 export const PackList = () => {
+
+    const [packs, setPacks] = useState([
+        {id: 1, name: 'Pack name', cards: 4, updates: '18.03.2021', created: 'Ivan Ivanov'},
+        {id: 1, name: 'Pack name', cards: 4, updates: '18.03.2021', created: 'Ivan Ivanov'},
+        {id: 1, name: 'Pack name', cards: 4, updates: '18.03.2021', created: 'Ivan Ivanov'},
+        {id: 1, name: 'Pack name', cards: 4, updates: '18.03.2021', created: 'Ivan Ivanov'},
+        {id: 1, name: 'Pack name', cards: 4, updates: '18.03.2021', created: 'Ivan Ivanov'},
+    ]);
+
     return (
         <div className={style.packList__container}>
-            <div>
-                <span>Name</span>
-                <span>Cards</span>
-                <span>Last Updated</span>
-                <span>Created by</span>
-                <span>Actions</span>
+            <div className={style.packList__row}>
+                <span className={style.packList__name}>Name</span>
+                <span className={style.packList__cards}>Cards</span>
+                <span className={style.packList__updates}>Last Updated</span>
+                <span className={style.packList__create}>Created by</span>
+                <span className={style.packList__action}>Actions</span>
             </div>
+            {
+                packs && packs.map(p => {
+                    return (
+                        <div
+                            key={p.id}
+                            className={style.packList__list}
+                        >
+                            <span className={style.packList__name}>{p.name}</span>
+                            <span className={style.packList__cards}>{p.cards}</span>
+                            <span className={style.packList__updates}>{p.updates}</span>
+                            <span className={style.packList__create}>{p.created}</span>
+                            <span className={style.packList__action}>
+                                <SuperButton className={style.packList__button_delete}>
+                                    Delete
+                                </SuperButton>
+                                <SuperButton className={style.packList__button_edit_learn}>
+                                    Edit
+                                </SuperButton>
+                                <SuperButton className={style.packList__button_edit_learn}>
+                                    Learn
+                                </SuperButton>
+                            </span>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
