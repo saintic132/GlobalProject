08e37756988c3575c3c25f7a989d10b0101f0ac6
gomelkodarya <gomelkodarya@gmail.com>
2022-05-30 21:59:09 +0300

    update JSX

--- src/components/features/Login/Login.tsx
+++ src/components/features/Login/Login.tsx
@@ -3,16 +3,22 @@ import SuperCheckbox
     from "../../../common/buttons/c3-SuperCheckbox/SuperCheckbox";
 import SuperButton from "../../../common/buttons/c2-SuperButton/SuperButton";
 import style from './Login.module.css'
+import {ForgotPass} from "../ForgotPass/ForgotPass";
 
 export const Login = () => {
     return (
         <div>
+            <h1>It-incubator</h1>
+            <h2>Sign In</h2>
             <form className={style.form}>
                 <SuperInputText />
                 <SuperInputText />
                 <SuperCheckbox />
+                <ForgotPass />
                 <SuperButton />
             </form>
+            <div><a href={'#'}>Don’t have an account?</a></div>
+            <div><a href={'#'}>Sign Up</a></div>
         </div>
     )
 };
