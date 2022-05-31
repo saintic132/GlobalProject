a6773f3edf6195bad8e8d9e9fa3c0c313416af0c
gomelkodarya <gomelkodarya@gmail.com>
2022-05-31 12:32:35 +0300

    create auth-reducer

---
+++ src/store/reducers/auth-reducer.ts
@@ -0,0 +1,11 @@
+const initialState = {
+    isLoggedIn: false
+}
+
+export const authReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+
+        default:
+            return state
+    }
+}
