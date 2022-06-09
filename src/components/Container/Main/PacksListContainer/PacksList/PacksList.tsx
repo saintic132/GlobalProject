5d0c1b82988a65de731bd446308f85c0f9ab322e
gomelkodarya <gomelkodarya@gmail.com>
2022-06-09 10:17:29 +0300

    add button and style for button

--- src/components/Container/Main/PacksListContainer/PacksList/PacksList.tsx
+++ src/components/Container/Main/PacksListContainer/PacksList/PacksList.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import SuperInputText from "../../../../../common/buttons/c1-SuperInputText/SuperInputText";
 import style from "./PacksList.module.css";
 import {PackList} from "./PackList/PackList";
+import SuperButton
+    from "../../../../../common/buttons/c2-SuperButton/SuperButton";
 
 export const PacksList = () => {
     return (
@@ -9,11 +11,14 @@ export const PacksList = () => {
             <h2>
                 Packs list
             </h2>
-            <SuperInputText
-                className={style.profile__body_input_search}
-                placeholder="Search..."
-                // value={email}
-            />
+            <div className={style.packsList__container}>
+                <SuperInputText
+                    className={style.packsList__body_input_search}
+                    placeholder="Search..."
+                    // value={email}
+                />
+                <SuperButton className={style.packsList__btn}>Add new pack</SuperButton>
+            </div>
             {/*Компонента с отрисовкой Карточек*/}
             <PackList/>
         </>
