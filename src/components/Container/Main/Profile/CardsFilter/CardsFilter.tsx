bface095ec68e147d106f5669c200f1662bbc53e
saintic <saintic132@gmail.com>
2022-06-10 14:25:55 +0300

    Сделана компонента Profile с отображением Packs по пользователю. Добавлена common компонента useDebounce для задержки при написании поискового запроса

--- src/components/Container/Main/Profile/CardsFilter/CardsFilter.tsx
+++ src/components/Container/Main/Profile/CardsFilter/CardsFilter.tsx
@@ -14,14 +14,15 @@ export const CardsFilter = memo(() => {
     const maxCardsCount = useAppSelector(state => state.packs.maxCardsCount)
     const sortPacks = useAppSelector(state => state.packs.sortPacks)
     const pageCount = useAppSelector(state => state.packs.pageCount)
-    const page = useAppSelector(state => state.packs.page)
+    const searchText = useAppSelector(state => state.packs.searchText)
 
     const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
     const dispatch = useAppDispatch()
 
+
     const changeMinValueCount = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setErrorMessage(null)
+        if (errorMessage) setErrorMessage(null)
         let value = +e.currentTarget.value;
         if (!isFinite(+value)) return;
         if (value > maxCardsCount) {
@@ -32,7 +33,7 @@ export const CardsFilter = memo(() => {
     }
 
     const changeMaxValueCount = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setErrorMessage(null)
+        if (errorMessage) setErrorMessage(null)
         let value = +e.currentTarget.value
         if (!isFinite(+value)) return;
         if (value > 110) {
@@ -42,30 +43,41 @@ export const CardsFilter = memo(() => {
         }
     }
 
+    const onClickApplyFilterValue = () => {
+        if (errorMessage) {
+            setErrorMessage(null)
+        }
+        dispatch(getPacksTC(searchText, sortPacks, 1, pageCount))
+    }
+
     return (
-        <div className={style.userProfile__params}>
+        <div className={style.cardsFilter__params}>
             <h4>Number of cards</h4>
-            <div className={style.userProfile__params_inputs}>
+            <div className={style.cardsFilter__params_inputs}>
                 <input
-                    className={style.userProfile__params_input}
+                    className={style.cardsFilter__params_input}
                     type='number'
                     value={Number(minCardsCount).toString()}
                     onChange={changeMinValueCount}
                     placeholder='от'
                 />
                 <input
-                    className={style.userProfile__params_input}
+                    className={style.cardsFilter__params_input}
                     type='number'
                     value={Number(maxCardsCount).toString()}
                     onChange={changeMaxValueCount}
                     placeholder='до'
                 />
             </div>
-            <SuperButton onClick={() => dispatch(getPacksTC(sortPacks, page, pageCount))}>
-                Filter
-            </SuperButton>
+            <div className={style.cardsFilter__buttonFilter_container}>
+                <SuperButton
+                    className={style.cardsFilter__button_filter}
+                    onClick={onClickApplyFilterValue}>
+                    Filter
+                </SuperButton>
+            </div>
             {
-                errorMessage && <div>{errorMessage}</div>
+                errorMessage && <div className={style.cardsFilter__server_error}>{errorMessage}</div>
             }
         </div>
     )
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, Store, legacy_createStore as createStore} from "redux";
+import {applyMiddleware, combineReducers, Store, legacy_createStore as createStore, compose} from "redux";
 import thunk, { ThunkDispatch } from "redux-thunk";
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 import {ProfileActionsType, profileReducer} from "./reducers/profile-reducer";
@@ -10,13 +10,15 @@ export type ActionsType = ProfileActionsType | PacksActionsType
 export type ReduxStateType = ReturnType<typeof rootReducer>
 export type TypedDispatch = ThunkDispatch<ReduxStateType, any, ActionsType>
 
+//@ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 let rootReducer = combineReducers({
     profile: profileReducer,
     packs: packsReducer
 })
 
-export const store: Store<ReduxStateType, ActionsType> = createStore(rootReducer, applyMiddleware(thunk))
+export const store: Store<ReduxStateType, ActionsType> = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 export const useAppDispatch = () => useDispatch<TypedDispatch>()
 export const useAppSelector: TypedUseSelectorHook<ReduxStateType> = useSelector
